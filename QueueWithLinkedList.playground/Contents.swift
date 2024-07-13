cab1fb7058cd284eba230fddb3ff8bc1ceeac7a8
Andre Dias <andrediasgustavo@gmail.com>
2024-07-13 15:29:00 -0300

    Add more context and info for the Queue created with linked list

--- QueueWithLinkedList.playground/Contents.swift
+++ QueueWithLinkedList.playground/Contents.swift
@@ -1,11 +1,22 @@
 //MARK: Queue Data structure
 
 /*
- Queue is a data structure that it is basically a linked list,
- with a head and tail, so you will usually have in this data structure
- a enQueue and deQeueu method where it with the enqueue method it add to
- the tail of the collection, and dequeue it is popped from the head, so it
- follows the FIFO order, first in first out.
+ Queue is a data structure that is basically a linked list,
+ with a head and a tail, so you will usually have in this data structure
+ an enQueue and deQeueu method, where with the enqueue method it is added to the
+ tail of the collection, and dequeue it is popped from the head, so it follows the
+ FIFO order, first in first out.
+ 
+ The question is when to use queue built with linked list, why or why not:
+ Advantage of O(1) for enqueue and dequeue, it only needs to have the
+ reference to the tail and head.
+ Linked list can grow dynamically without any problem, no overcost for growing like an array has.
+ Disadvantages are: A bit more memory usage because of maintaining the next node reference.
+ Not so good for caching due to linked list nature to be scattered in memory.
+ So concluding that it is good to use if you need constant complexity in enqueue 
+ and dequeue operations, memory overhead for linking references between nodes is not a
+ problem and/or the queue can grow unexpectedly.
+
 */
 
 
