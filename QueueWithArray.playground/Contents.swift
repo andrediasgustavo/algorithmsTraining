626158b20dde64921cbf7d6297831aca67f350de
Andre Dias <andrediasgustavo@gmail.com>
2024-07-13 15:38:49 -0300

    Add more context and info for the Queue created with array

--- QueueWithArray.playground/Contents.swift
+++ QueueWithArray.playground/Contents.swift
@@ -1,11 +1,21 @@
 //MARK: Queue Data structure
 
 /*
- Queue is a data structure created using an array with a head and tail,
- so you will usually have in this data structure
- a enQueue and deQeueu method where it with the enqueue method it add to
- the tail of the collection, and dequeue it is popped from the head, so it
- follows the FIFO order, first in first out.
+ Queue is a data structure created using an array with a head and a tail,
+ So you will usually have in this data structure an enQueue and deQeueu method,
+ where with the enqueue method it will add to the tail of the collection, and dequeue
+ it will be popped from the head, so it follows the FIFO order, first in first out.
+ 
+The question is when to use a queue built with arrays, why or why not:
+ Arrays have better cache efficiency due to their contiguous memory allocation.
+ Arrays are also easier to use and have a lot of built-in tools in the standard swift sdk.
+ The problems are when enqueue and dequeue are O(n) time complexity operations, because 
+ of the shifting of elements in the collection when an element is added or removed.
+ Also the resizing of the array when it grows to unexpected size, it has the cost of 
+ allocating more contiguous pace in memory, so it is usually an O(n) operation, of course
+ that swift for example have a lot of optimizations but it is more costly compared to linked list for example.
+ So concluding that it's a good use when caching is paramount and/or the size won't grow unexpectedly.
+ 
 */
 
 
